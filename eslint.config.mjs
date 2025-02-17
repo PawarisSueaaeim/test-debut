7fd4ce9983f89446c65f11f37f4365729a7f8db8
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2025-02-17 15:13:18 +0700

    fix: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,21 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // ปิด warning ของ react-hooks/exhaustive-deps
+      "react-hooks/exhaustive-deps": "off",
+
+      // อนุญาตให้ใช้ {} เป็น type
+      "@typescript-eslint/no-empty-object-type": "off",
+
+      // อนุญาตให้ใช้ any
+      "@typescript-eslint/no-explicit-any": "off",
+
+      // ปิด error ที่เกิดจากตัวแปรที่ไม่ได้ใช้
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
